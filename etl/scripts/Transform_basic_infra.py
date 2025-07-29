9c2779559619ce19d9b3b1549e33fab9edfcc1f0
brennohdev <196482934+brennohdev@users.noreply.github.com>
2025-07-29 02:02:20 +0000

    style(etl): Formata código Python automaticamente ✨

--- etl/scripts/Transform_basic_infra.py
+++ etl/scripts/Transform_basic_infra.py
@@ -1,16 +1,19 @@
-
 import pandas as pd
 
-def calculate_basic_infra_percent(df_paraiba, infra_basicas):
 
+def calculate_basic_infra_percent(df_paraiba, infra_basicas):
     porcentagem_escolas_sem_infra_basica = []
 
     for codigo, nome in infra_basicas.items():
         total = len(df_paraiba)
-        sem_infra = len(df_paraiba[df_paraiba[codigo] == 0 ])
+        sem_infra = len(df_paraiba[df_paraiba[codigo] == 0])
         percentual_sem_infra = (sem_infra / total) * 100
-        porcentagem_escolas_sem_infra_basica.append({ 'Item': nome, 'Faltam (%)': percentual_sem_infra, 'Total': total})
-    
-    df_resultado_porcentagem_escolas_sem_infra_basica = pd.DataFrame(porcentagem_escolas_sem_infra_basica).sort_values('Faltam (%)', ascending=False)
+        porcentagem_escolas_sem_infra_basica.append(
+            {"Item": nome, "Faltam (%)": percentual_sem_infra, "Total": total}
+        )
+
+    df_resultado_porcentagem_escolas_sem_infra_basica = pd.DataFrame(
+        porcentagem_escolas_sem_infra_basica
+    ).sort_values("Faltam (%)", ascending=False)
 
     return df_resultado_porcentagem_escolas_sem_infra_basica
