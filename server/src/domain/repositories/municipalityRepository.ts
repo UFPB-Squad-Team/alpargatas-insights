d26adc4c7baa16166791374ef4b0bfedbf987d82
SamuelColaco <166066149+SamuelColaco@users.noreply.github.com>
2025-07-29 17:11:36 +0000

    chore(server): auto-format TypeScript code via CI

--- server/src/domain/repositories/municipalityRepository.ts
+++ server/src/domain/repositories/municipalityRepository.ts
@@ -1,12 +1,12 @@
-import { Municipality } from "../entities/municipality";
-import { UF } from "../enums/enumUnidadesFederativas";
+import { Municipality } from '../entities/municipality';
+import { UF } from '../enums/enumUnidadesFederativas';
 
-export interface IMunicipalityRepository{
-    findById(id: string): Promise<Municipality | null>
-    findByName(name: string): Promise<Municipality |  null>
-    findByUf(uf: UF): Promise<Municipality | null>
-    findAll(): Promise<Municipality[]>
-    delete(id: string): Promise<void>
-    update(municipality: Municipality): Promise<void>
-    save(municipality: Municipality): Promise<void>
+export interface IMunicipalityRepository {
+  findById(id: string): Promise<Municipality | null>;
+  findByName(name: string): Promise<Municipality | null>;
+  findByUf(uf: UF): Promise<Municipality | null>;
+  findAll(): Promise<Municipality[]>;
+  delete(id: string): Promise<void>;
+  update(municipality: Municipality): Promise<void>;
+  save(municipality: Municipality): Promise<void>;
 }
--- server/src/domain/repositories/schoolRepository.ts
+++ server/src/domain/repositories/schoolRepository.ts
@@ -1,15 +1,16 @@
-import { School } from "../entities/school";
-import { dependenciaAdministrativa } from "../enums/enumDependenciaAdministrativa";
-import { UF } from "../enums/enumUnidadesFederativas";
+import { School } from '../entities/school';
+import { dependenciaAdministrativa } from '../enums/enumDependenciaAdministrativa';
+import { UF } from '../enums/enumUnidadesFederativas';
 
-export interface ISchoolRepository{
-    findById(id: string): Promise<School | null>
-    findByName(name: string): Promise<School | null>
-    findByUf(uf: UF): Promise<School | null>
-    findByDepAdm(dependenciaAdministrativa: dependenciaAdministrativa): Promise<School | null>
-    findAll(): Promise<School[]>
-    delete(id: string): Promise<void>
-    update(school: School): Promise<void>
-    save(school: School): Promise<void>
-
+export interface ISchoolRepository {
+  findById(id: string): Promise<School | null>;
+  findByName(name: string): Promise<School | null>;
+  findByUf(uf: UF): Promise<School | null>;
+  findByDepAdm(
+    dependenciaAdministrativa: dependenciaAdministrativa,
+  ): Promise<School | null>;
+  findAll(): Promise<School[]>;
+  delete(id: string): Promise<void>;
+  update(school: School): Promise<void>;
+  save(school: School): Promise<void>;
 }
