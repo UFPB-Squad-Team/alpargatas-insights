c2e0bfac37affe125bcd050832c4d83df8645b3e
Samuel Colaço <166066149+SamuelColaco@users.noreply.github.com>
2025-07-30 18:50:20 -0300

    [Domain]feat:  Add DomainRepositories for school and municipality
    * feature: Add DomainRepositories for school and municipality
    
    * chore(server): auto-format TypeScript code via CI
    
    * feat: Add changes in repos
    
    * chore(server): auto-format TypeScript code via CI

---
+++ server/src/domain/repositories/municipalityRepository.ts
@@ -0,0 +1,12 @@
+import { Municipality } from '../entities/municipality';
+import { UF } from '../enums/enumUnidadesFederativas';
+
+export interface IMunicipalityRepository {
+  findById(id: string): Promise<Municipality | null>;
+  findByName(name: string): Promise<Municipality | null>;
+  findByUf(uf: UF): Promise<Municipality[]>;
+  findAll(): Promise<Municipality[]>;
+  delete(id: string): Promise<void>;
+  update(municipality: Municipality): Promise<void>;
+  save(municipality: Municipality): Promise<void>;
+}
