3210b76cd57d32df317e5325bb835706c4fc650a
yScroww <163170274+yScroww@users.noreply.github.com>
2025-08-09 01:00:47 +0000

    chore(etl): auto-format Python code via CI

--- etl/scripts/load.py
+++ etl/scripts/load.py
@@ -1,9 +1,10 @@
 import logging
-from pymongo import MongoClient
-import pandas as pd
 import os
 from pathlib import Path
+
 import numpy as np
+import pandas as pd
+from pymongo import MongoClient
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,8 +12,7 @@ logging.basicConfig(
 )
 
 
-def load_to_mongo (df: pd.DataFrame, db_name: str, collection_name: str, uri: str):
-    
+def load_to_mongo(df: pd.DataFrame, db_name: str, collection_name: str, uri: str):
     """Carrega um DataFrame do Pandas para uma coleção do MongoDB."""
 
     try:
@@ -25,22 +25,25 @@ def load_to_mongo (df: pd.DataFrame, db_name: str, collection_name: str, uri: st
         if df.empty:
             logging.error("O DataFrame está vazio. Nenhum dado será inserido.")
             return
-        
-        data_to_load = df.to_dict(orient='records')
+
+        data_to_load = df.to_dict(orient="records")
 
         if data_to_load:
             collection.delete_many({})
 
             result = collection.insert_many(data_to_load)
-            logging.info(f"Load do ETL concluído: {len(result.inserted_ids)} documentos inseridos na coleção '{collection_name}'.")         
+            logging.info(
+                f"Load do ETL concluído: {len(result.inserted_ids)} documentos inseridos na coleção '{collection_name}'."
+            )
         else:
             logging.error("Nenhum dado para inserir.")
     except Exception as e:
         logging.error(f"Erro durante o Load para o MongoDB: {e}")
     finally:
-        if 'client' in locals() and client:
+        if "client" in locals() and client:
             client.close()
-            logging.info("Conexão com o MongoDB fechada.")    
+            logging.info("Conexão com o MongoDB fechada.")
+
 
 def main():
     base_path = Path(__file__).resolve().parents[1]
@@ -56,21 +59,28 @@ def main():
 
             logging.info("DataFrame carregado com sucesso.")
 
-            logging.info("Convertendo a coluna 'localizacao' para a formatação ideal para o MongoDB")
-            df_escolas['localizacao'] = df_escolas['localizacao'].apply(
-                lambda x: {**x, 'coordinates': x['coordinates'].tolist()} 
-                if isinstance(x['coordinates'], np.ndarray) else x)
-            
+            logging.info(
+                "Convertendo a coluna 'localizacao' para a formatação ideal para o MongoDB"
+            )
+            df_escolas["localizacao"] = df_escolas["localizacao"].apply(
+                lambda x: {**x, "coordinates": x["coordinates"].tolist()}
+                if isinstance(x["coordinates"], np.ndarray)
+                else x
+            )
+
             uri_mongo = "mongodb+srv://alpargatas_user:hGJZbmrO46NPksBb@alpargatas-insights-clu.2iuzc4b.mongodb.net/?retryWrites=true&w=majority&appName=alpargatas-insights-cluster"
             db_mongo = "teste_load"
             collection_mongo = "teste1"
 
             logging.info("Inserindo os dados no MongoDB")
-            load_to_mongo(df = df_escolas, db_name=db_mongo, collection_name=collection_mongo, uri=uri_mongo)
+            load_to_mongo(
+                df=df_escolas,
+                db_name=db_mongo,
+                collection_name=collection_mongo,
+                uri=uri_mongo,
+            )
         except Exception as e:
             logging.error(f"Ocorreu um erro ao processar o arquivo: {e}")
 
+
 main()
-
-
-    
