561a28dbe4d438910dbd02b1a9512c17c2499606
Samuel Colaço <166066149+SamuelColaco@users.noreply.github.com>
2025-09-16 10:44:06 -0300

    [API] feat: Add new entity, repo and schema for needs (#73)
    * feat: Add new entity, repo and schema for needs
    
    * chore(server): auto-format TypeScript code via CI

---
+++ server/src/domain/entities/need.ts
@@ -0,0 +1,94 @@
+import { randomUUID } from 'node:crypto';
+import { NeedStatus } from '../enums/Need/enumNeedStatus';
+import { NeedType } from '../enums/Need/enumNeedType';
+import { SubmitterType } from '../enums/Need/enumSubmitterType';
+
+/**
+ * @description Interface representing a need entry.
+ */
+export type NeedProps = {
+  id: string;
+
+  title: string;
+
+  description: string;
+
+  type: NeedType;
+
+  submitterType: SubmitterType;
+
+  submitterContact?: { name?: string; email?: string };
+
+  location?: {
+    locationType: 'school' | 'municipality';
+    id: string;
+    name: string;
+  };
+
+  status: NeedStatus;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
+};
+
+export class Need {
+  public readonly id: string;
+
+  public title: string;
+
+  public description: string;
+
+  public type: NeedType;
+
+  public submitterType: SubmitterType;
+
+  public submitterContact?: { name?: string; email?: string };
+
+  public location?: {
+    locationType: 'school' | 'municipality';
+    id: string;
+    name: string;
+  };
+
+  public status: NeedStatus;
+
+  public createdAt?: Date;
+
+  public updatedAt?: Date;
+
+  constructor(
+    {
+      title,
+      description,
+      type,
+      submitterType,
+      submitterContact,
+      location,
+      status,
+      createdAt,
+      updatedAt,
+    }: NeedProps,
+    id?: string,
+  ) {
+    this.id = id ?? randomUUID();
+
+    this.title = title.trim();
+
+    this.description = description.trim();
+
+    this.type = type;
+
+    this.submitterType = submitterType;
+
+    this.submitterContact = submitterContact;
+
+    this.location = location;
+
+    this.status = status ?? NeedStatus.PENDING;
+
+    this.createdAt = createdAt;
+
+    this.updatedAt = updatedAt;
+  }
+}
---
+++ server/src/domain/repositories/needRepository.ts
@@ -0,0 +1,15 @@
+import { Need } from '../entities/need';
+
+export interface INeedRepository {
+  listApproved(
+    page: number,
+    limit: number,
+    filters?: Partial<Need>,
+  ): Promise<{
+    needs: Need[];
+    page: number;
+    total: number;
+    currentPage: number;
+  }>;
+  save(need: Need): Promise<Need>;
+}
