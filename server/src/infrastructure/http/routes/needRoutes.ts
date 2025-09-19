2f8c4ddc2be187e48eb0e2c033d495a11bd6f863
Samuel Colaço <166066149+SamuelColaco@users.noreply.github.com>
2025-09-19 10:55:49 -0300

    [API] feat: Add new get details need (#77)
    * feat: Add new get details need
    
    * chore(server): auto-format TypeScript code via CI
    
    ---------
    
    Co-authored-by: Brenno Henrique <brennohdev@gmail.com>

--- server/src/infrastructure/http/routes/needRoutes.ts
+++ server/src/infrastructure/http/routes/needRoutes.ts
@@ -11,6 +11,13 @@ needRoutes.get(
   ),
 );
 
+needRoutes.get(
+  '/api/v1/needs/:id',
+  needController.listNeedDetailsController.listDetails.bind(
+    needController.listNeedDetailsController,
+  ),
+);
+
 needRoutes.post(
   '/api/v1/needs',
   needController.createNeedController.create.bind(
